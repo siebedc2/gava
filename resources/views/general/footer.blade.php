96cfe1682a6d9ad8d0b4692002bbcc9aaf61e720
Siebe <siebedc2@gmail.com>
2020-02-26 21:50:50 +0100

    Started making the landing page

---
+++ resources/views/general/footer.blade.php
@@ -0,0 +1,39 @@
+<div class="footer-wrapper">
+    <div class="container text-white">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <!-- Begin Mailchimp Signup Form -->
+                <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+
+                <div id="mc_embed_signup">
+                    <form action="https://thomasmore.us20.list-manage.com/subscribe/post?u=3b548efea3d3b4632e73d89fd&amp;id=69946914f5" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" rel="noopener" novalidate>                     <div id="mc_embed_signup_scroll">
+                            <h2>Follow our news and updates. It’s free!</h2>
+                            <!-- <div class="indicates-required"><span class="asterisk">*</span> indicates required</div> -->
+                            <div class="mc-field-group">
+                                <!--<label for="mce-EMAIL">Email Address <span class="asterisk">*</span></label>-->
+                                <input placeholder="email" type="email" value="" name="EMAIL" class="rounded-pill required email" id="mce-EMAIL">
+                            </div>
+
+                            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_3b548efea3d3b4632e73d89fd_69946914f5" tabindex="-1" value=""></div>
+                            <div class="submitbutton clear"><input type="submit" value="subscribe" name="subscribe" id="mc-embedded-subscribe" class="rounded-pill btn btn-secondary"></div>
+
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>
+                        </div>
+
+                    </form>
+                </div>
+                <!--End mc_embed_signup-->
+            </div>
+
+            <div class="col-12 col-md-6">
+                <a rel="noopener" target="_blank" href="https://www.facebook.com/GavaLearn/"><img src="img/facebook.png" alt="facebook"></a>
+                <a rel="noopener" target="_blank" href="https://twitter.com/gava_learning?s=20"><img src="img/twitter.png" alt="twitter"></a>
+                <a rel="noopener" target="_blank" href="https://www.instagram.com/gavalearn/?hl=nl"><img src="img/instagram.png" alt="instagram"></a>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,54 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- OG - tags facebook -->
+    <meta property="og:url" content="http://gava-learning.be/" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Gava" />
+    <meta property="og:description" content="Peer to peer platform to keep up with the latest technologies.">
+    <meta property="og:image" content="https://gava-learning.be/img/twittercard.png" />
+    <meta property="fb:app_id" content="444821389748822" />
+    
+    <!-- Twitter cards -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@gavalearn" />
+    <meta name="twitter:title" content="Gava" />
+    <meta name="twitter:description" content="Peer to peer platform to keep up with the latest technologies.">
+    <meta name="twitter:image" alt="twitter" content="https://gava-learning.be/img/twittercard.png" />
+    <meta name="twitter:url" content="https://gava-learning.be/">
+    <meta name="twitter:card" content="summary_large_image">
+    
+    <meta name="language" content="EN">
+    <meta name="description" content="Gava learning">
+    <meta name="keywords" content="gava, learn, teach, innovate, online, learning, technology, peer, social">
+    <meta name="reply-to" content="gavalearn@gmail.com">
+    <meta name="author" content="Siebe De Celle">
+    <meta name="rating" content="general">
+    <meta name="rating" content="safe for kids">
+    <meta name="robots" content="index, nofollow">
+    <meta name="robots" content="noydir">
+    <meta name="googlebot" content="noodp">
+    <meta name="googlebot" content="noarchive">
+    <meta name="googlebot" content="nosnippet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description" content="Gava is focusing on the technology sectors. These have been in strong growth for some time and don't immediately think about a coffee break. It is therefore important for people in this sector or with interest to stay informed as much as possible.">
+    
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Gava - Learn, teach, innovate</title>
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="css/app.css">
+    @yield('extra-css')
+    
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+    <link rel="icon" href="img/favicon.png" type="image/x-icon">
+    
+    <!-- Canonical tag -->
+    <link rel="canonical" href="https://gava-learning.be/">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+<body class="gava">
+    @yield('content')
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+    @yield('extra-js')
 </body>
 </html>
